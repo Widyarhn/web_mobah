98f6c48924f1e081a7f44ce2326099eaa728ed27
olivvmj <jannahvvmj@gmail.com>
2023-04-05 10:13:01 +0700

    pengisian model

--- app/Models/Validator.php
+++ app/Models/Validator.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Validator extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+    protected $table = 'validator';
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
